e18ae9cf2a213a46359448265ecf8f4762631b9d
Tom <tomaszzf@gmail.com>
2021-10-31 21:03:53 +0000

    Add loginCallback and verifyLogin routes

--- domains/gmail/controller.js
+++ domains/gmail/controller.js
@@ -24,6 +24,20 @@ const GmailController = (app, googleCredentials) => {
         });
         res.send(JSON.stringify(authUrl));
     });
-}
 
-export default GmailController;
+    app.get('/gmail/loginCallback', (req, res) => {
+        console.log(req.url);
+        //Get the tokens from the code
+        //Send back a script to close the popup and return to the app
+        res.send("<script>window.close('','_parent','');</script>");
+    });
+ 
+    app.get('/gmail/verifyLogin', (req,res) => {
+        //TODO:: Should return if the user is logged and it's details
+        //TODO:: Consider creating user domain shared with other login methods
+        res.send("OK");
+    });
+    
+};
+
+module.exports = GmailController
