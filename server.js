f23f53c3d4fb5284d08cd6dbcf0b392160869e06
Tom <tomaszzf@gmail.com>
2021-10-31 14:54:58 +0000

    Add region comments

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+//--Express--//
 const app = require('express')();
 const port = 3001;
 
+//--Cors--//
 //required for cross origin requests
 var corsOptions = {
     origin: 'http://localhost:3000',
@@ -8,14 +10,17 @@ var corsOptions = {
   }
 const cors = require('cors')(corsOptions);
 
+//--Express Middleware--//
 app.use(cors);
 app.use(express.json());
 
+
+//--Routes--//
 app.get('/ping', (req, res) => {
     res.send(pong);
 })
 
-
+//--Start Server--//
 app.listen(port, () => {
     console.log(`Listening on port ${port}!`)
 });
